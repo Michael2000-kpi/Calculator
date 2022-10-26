60e345d8905d4b74d5983c536ff460575397696e
Michael2000-kpi <85711932+Michael2000-kpi@users.noreply.github.com>
2022-10-27 01:37:55 +0300

    calculator

--- src/App.js
+++ src/App.js
@@ -2,32 +2,66 @@ import React, { useState } from "react";
 import "./App.css";
 
 function App() {
-  const [counter, setCounter] = useState(0);
+  const [data, setData] = useState("");
+  const calcBtns = [];
+  [9, 8, 7, 6, 5, 4, 3, 2, 1, 0, ",", ".", "%"].forEach((item) => {
+    calcBtns.push(
+      <button
+        onClick={(e) => setData(data + e.target.value)}
+        value={item}
+        key={item}
+      >
+        {item}
+      </button>
+    );
+  });
   return (
-    <>
-      <nav
-        style={{
-          background: "#333",
-        }}
-      >
-        <ul>
-          <li>
-            <button onClick={() => setCounter(counter + 1)}>Increment</button>
-          </li>
-          <li>
-            <button onClick={() => setCounter(counter - 1)}>Decrement</button>
-          </li>
-        </ul>
-      </nav>
-      <div
-        style={{
-          textAlign: "center",
-          background: "#455",
-        }}
-      >
-        <p>Count {counter}</p>
+    <div className="wrapper">
+      <div className="show-input">{data}</div>
+
+      <div className="modifiers subgrid">
+        <button
+          onClick={() => {
+            setData(data.substring(0, data.length - 1));
+          }}
+        >
+          Стерти/Clear
+        </button>
+        <button onClick={() => setData("")}>AC</button>
       </div>
-    </>
+      <div className="operations subgrid direction">
+        <button onClick={(e) => setData(data + e.target.value)} value="+">
+          +
+        </button>
+        <button onClick={(e) => setData(data + e.target.value)} value="-">
+          -
+        </button>
+        <button onClick={(e) => setData(data + e.target.value)} value="*">
+          *
+        </button>
+        <button onClick={(e) => setData(data + e.target.value)} value="/">
+          /
+        </button>
+        <button
+          onClick={(e) => {
+            try {
+              setData(
+                String(eval(data)).length > 3 &&
+                  String(eval(data)).includes(".")
+                  ? String(eval(data).toFixed(4))
+                  : String(eval(data))
+              );
+            } catch (err) {
+              console.log(err);
+            }
+          }}
+          value="="
+        >
+          =
+        </button>
+      </div>
+      <div className="digits flex">{calcBtns}</div>
+    </div>
   );
 }
 
